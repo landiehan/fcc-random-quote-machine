107acd9b878a6df33913f68d0fb773d67540b815
Landay <67254560+landiehan@users.noreply.github.com>
2021-02-05 22:57:44 +0800

    Update App.js
    fetch quotes only after the first render, and pass that quotes data to QuoteBox component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import QuoteBox from "./components/quoteBox";
+
+const url =
+  "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
 function App() {
+  const [quotes, setQuotes] = useState(null);
+  useEffect(() => {
+    const getQuotes = async () => {
+      const response = await fetch(url);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        setQuotes(jsonResponse.quotes);
+      }
+    };
+    getQuotes();
+  }, []);
+
+  if (!quotes) {
+    return <p style={{ fontSize: "64px", color: "red" }}>Loading......</p>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random Quote Machine</h1>
+      <QuoteBox quotes={quotes} />
     </div>
   );
 }
